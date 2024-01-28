951d8d5fe7fdf7f3ce32140989b3a964d60c6d24
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-01-28 12:19:55 -0500

    try with submit

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-// import { getSSLHubRpcClient, Message } from "@farcaster/hub-nodejs";
+const { getSSLHubRpcClient, Message } = require("@farcaster/hub-nodejs");
 const express = require('express');
 const app = express();
 
-// const HUB_URL = process.env['HUB_URL'] || "nemes.farcaster.xyz:2283";
-// const client = getSSLHubRpcClient(HUB_URL);
+const HUB_URL = process.env['HUB_URL'] || "nemes.farcaster.xyz:2283";
+const client = getSSLHubRpcClient(HUB_URL);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
@@ -30,41 +30,38 @@ app.get('/base', (req, res) => {
   `);
 });
 
-// app.post('/submit', async (req, res) => {
-//   try {
-//     const frameMessage = Message.decode(Buffer.from(req.body?.trustedData?.messageBytes || '', 'hex'));
-//     const result = await client.validateMessage(frameMessage);
-//     if (result.isOk() && result.value.valid) {
-//       const validatedMessage = result.value.message;
-//       // Process the validated message here
-//       res.send('Message validated successfully');
-//     } else {
-//       res.status(400).send('Invalid message');
-//     }
-//   } catch (e) {
-//     res.status(500).send(`Failed to validate message: ${e}`);
-//   }
+app.post('/submit', async (req, res) => {
+  const frameMessage = Message.decode(Buffer.from(req.body?.trustedData?.messageBytes || '', 'hex'));
+  const result = await client.validateMessage(frameMessage);
+  if (result.isOk() && result.value.valid) {
+    const validatedMessage = result.value.message;
+    console.log(validatedMessage)
+  }
+  else {
+    console.log(`Failed to validate message: ${result.error}`);
+    res.status(500).send(`Failed to validate message: ${e}`);
+  }
 
-//   // Return an HTML response
-//   res.setHeader('Content-Type', 'text/html');
-//   res.status(200).send(`
-//         <!DOCTYPE html>
-//         <html>
-//           <head>
-//             <title>EAS Submitted!</title>
-//             <meta property="og:title" content="EAS Submitted">
-//             <meta property="og:image" content="https://docs.attest.sh/img/eas-logo.png">
-//             <meta name="fc:frame" content="vNext">
-//             <meta name="fc:frame:image" content="https://docs.attest.sh/img/eas-logo.png">
-//             <meta name="fc:frame:button:1" content="Succesfully attested on Base">
-//           </head>
-//           <body>
-//             <p>Attestation submitted: transaction hash</p>
-//           </body>
-//         </html>
-//       `);
-// });
+  // Return an HTML response
+  res.setHeader('Content-Type', 'text/html');
+  res.status(200).send(`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>EAS Submitted!</title>
+            <meta property="og:title" content="EAS Submitted">
+            <meta property="og:image" content="https://docs.attest.sh/img/eas-logo.png">
+            <meta name="fc:frame" content="vNext">
+            <meta name="fc:frame:image" content="https://docs.attest.sh/img/eas-logo.png">
+            <meta name="fc:frame:button:1" content="Succesfully attested on Base">
+          </head>
+          <body>
+            <p>Attestation submitted: transaction hash</p>
+          </body>
+        </html>
+      `);
+});
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log(`app listening on port ${process.env.PORT || 5000}`);
+app.listen(process.env.PORT || 5001, () => {
+  console.log(`app listening on port ${process.env.PORT || 5001}`);
 });
