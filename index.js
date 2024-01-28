0af31fee9d6386d61bfcec91e6821e1f2353b75b
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-01-28 12:25:47 -0500

    fix submit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get('/base', (req, res) => {
         <meta property="og:image" content="https://docs.attest.sh/img/eas-logo.png">
         <meta name="fc:frame" content="vNext">
         <meta name="fc:frame:image" content="https://docs.attest.sh/img/eas-logo.png">
-        <meta name="fc:frame:post_url" content="${process.env['HOST']}/submit">
+        <meta name="fc:frame:post_url" content="https://frame-eas-a34243560586.herokuapp.com/submit">
         <meta name="fc:frame:button:1" content="I attest I saw this cast">
       </head>
       <body>
