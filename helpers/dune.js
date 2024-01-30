b550d562f4e1eea2d4bd30a7a7b544dbc8349c2f
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-01-29 22:32:59 -0500

    add auto refresh

--- helpers/dune.js
+++ helpers/dune.js
@@ -5,25 +5,43 @@ import { QueryParameter, DuneClient } from "@cowprotocol/ts-dune-client";
 import { Headers } from 'node-fetch';
 import fetch from 'node-fetch';
 
+// //QA only
+// let poll_data = [
+//     {text: '3.14', percentOfTotal: 0, votes: 0},
+//     {text: '42', percentOfTotal: 0, votes: 0},
+//     {text: '69', percentOfTotal: 0, votes: 0},
+//     {text: '1337', percentOfTotal: 0, votes: 0}
+// ]
+// const cast_hash = "0xdf95758ae0435328978871bf9960a5a8aba3010d"
+// //QA only
+
 const DUNE_API_KEY = process.env["DUNE_API_KEY"];
 
 export async function get_poll_data(cast_hash, poll_data) {
-    //try latest, and then refresh if latest doesn't work. TODO: add latest results endpoint to sdk
+    // try latest, and then refresh if latest doesn't work. TODO: add latest results endpoint to sdk
     let results = null
     try{
         const meta = {
             "x-dune-api-key": DUNE_API_KEY
         };
         const header = new Headers(meta);
-        const latest_response = await fetch('https://api.dune.com/api/v1/query/3389839/results', {
+        const latest_response = await fetch('https://api.dune.com/api/v1/query/3389839/results', { //TODO add in cast hash in param
             method: 'GET',
             headers: header,
         });
-        //TODO add in cast hash in param
 
         const body = await latest_response.text();
-        results = JSON.parse(body).result.rows[0].results;
+
+        //if execution is stale, refresh it
+        const executionEndedAt = JSON.parse(body).execution_ended_at;
+        const fiveMinutesAgo = new Date(Date.now() - 5 * 60 * 1000).toISOString();
+        if (executionEndedAt < fiveMinutesAgo) {
+            throw new Error("Execution ended more than five minutes ago.");
+        } else {
+            results = JSON.parse(body).result.rows[0].results;
+        }
     } catch (error) {
+        console.log(error)
         const client = new DuneClient(DUNE_API_KEY ?? "");
         const queryID = 3389839;
         const parameters = [
@@ -39,6 +57,7 @@ export async function get_poll_data(cast_hash, poll_data) {
         poll_data[i].percentOfTotal = JSON.parse(results[i]).percentOfTotal;
         poll_data[i].votes = JSON.parse(results[i]).votes;
     }
+    // console.log(poll_data)
     return poll_data
 }
 
