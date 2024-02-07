a8c4180d9c30b3109b6c71c5c13e96c1b2e3ebb2
Andrew <47720952+andrewhong5297@users.noreply.github.com>
2024-02-07 15:43:57 -0500

    should be better now

--- helpers/eas.js
+++ helpers/eas.js
@@ -12,36 +12,25 @@ const SYNDICATE_API_KEY = process.env['SYNDICATE_API_KEY'];
 
 //@todo: make this offchain EAS and then upload in bulk to dune
 export async function eas_mint(cast_hash, fid, attest_wallet, button_index, trusted_data, verifiable=false) {
-    // //push to EAS either onchain or offchain. docs: https://docs.attest.sh/docs/tutorials/make-an-attestation
-    // const provider = ethers.getDefaultProvider(
-    //     "base", {
-    //         alchemy: process.env['ALCHEMY_KEY']
-    //     }
-    // );
-    // const signer = new ethers.Wallet(process.env['PRIVATE_KEY'], provider);
-    // const eas = new EAS("0x4200000000000000000000000000000000000021"); //https://docs.attest.sh/docs/quick--start/contracts#base
-    // eas.connect(signer);
-
     // Initialize SchemaEncoder with the schema string
     cast_hash = cast_hash.startsWith('0x') ? cast_hash.substring(2) : cast_hash; //depending on source, sometimes hash has 0x in it.
     const padded_cast = Buffer.from(cast_hash + '0'.repeat(64 - cast_hash.length), 'hex')
     const schemaEncoder = new SchemaEncoder("bytes32 cast_hash, uint112 fid, uint8 button_index, bytes trusted_data");
     
     if (verifiable === false) {
-        trusted_data = trusted_data.substring(1, 4); // take only the first 4 bytes of trusted_data
         console.log("posting attestation with verifiable: false");
     }
     const encodedData = schemaEncoder.encodeData([
         { name: "cast_hash", value: padded_cast, type: "bytes32" },
         { name: "fid", value: fid, type: "uint112" },
         { name: "button_index", value: button_index, type: "uint8" },
-        { name: "trusted_data", value: Buffer.from(trusted_data, 'hex'), type: "bytes" }
+        { name: "trusted_data", value: Buffer.from(verifiable === false ? trusted_data.substring(1, 4) : trusted_data, 'hex'), type: "bytes" }
     ]);
+    // console.log(encodedData)
 
-    console.log(encodedData)
     const schemaUID = "0x6e333418327e1082bc2c5366560c703b447901a4b8d4ca9c754e9a8460eedbde";
-
-    const response = await axios.post(
+    // const response = await 
+    axios.post(
         "https://frame.syndicate.io/api/mint",
         {
           frameTrustedData: trusted_data,
@@ -66,9 +55,20 @@ export async function eas_mint(cast_hash, fid, attest_wallet, button_index, trus
           },
         }
       );
-    console.log(response.status, response.data)
-    return response
-
+    // console.log(response.status, response.data)
+    // return response
+    return { status: 200, data: "ok"}
+    
+    // //push to EAS either onchain or offchain. docs: https://docs.attest.sh/docs/tutorials/make-an-attestation
+    // const provider = ethers.getDefaultProvider(
+    //     "base", {
+    //         alchemy: process.env['ALCHEMY_KEY']
+    //     }
+    // );
+    // const signer = new ethers.Wallet(process.env['PRIVATE_KEY'], provider);
+    // const eas = new EAS("0x4200000000000000000000000000000000000021"); //https://docs.attest.sh/docs/quick--start/contracts#base
+    // eas.connect(signer);
+    // .....
     // const tx = await eas.attest({
     //     schema: schemaUID,
     //     data: {
@@ -86,13 +86,14 @@ export async function eas_mint(cast_hash, fid, attest_wallet, button_index, trus
 }
 
 export async function eas_check(cast_hash, attest_wallet) {
-    const provider = ethers.getDefaultProvider(
-        "base", {
-            alchemy: process.env['ALCHEMY_KEY']
-        }
-    );
-    const signer = new ethers.Wallet(process.env['PRIVATE_KEY'], provider);
-    const attesting = signer.address
+    // const provider = ethers.getDefaultProvider(
+    //     "base", {
+    //         alchemy: process.env['ALCHEMY_KEY']
+    //     }
+    // );
+    // const signer = new ethers.Wallet(process.env['PRIVATE_KEY'], provider);
+    // const attesting = signer.address
+    const attesting = "0x98407Cb54D8dc219d8BF04C9018B512dDbB96caB"
     const schema_id = '0x6e333418327e1082bc2c5366560c703b447901a4b8d4ca9c754e9a8460eedbde' //https://base.easscan.org/schema/view/0x6e333418327e1082bc2c5366560c703b447901a4b8d4ca9c754e9a8460eedbde
     const checksummed_wallet = getAddress(attest_wallet); //viem
     
